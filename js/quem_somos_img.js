3aa893667c16e0b825a21f70b6b44ccffd210970
lexgt1 <lexgt1.s@gmail.com>
2024-07-29 02:47:16 -0300

    Imagem carregada via Java script

---
+++ js/quem_somos_img.js
@@ -0,0 +1,8 @@
+document.getElementById("quemsomos-img").innerHTML = "<img src='./img/quemsomos/FOTO quem somos.png'>";
+document.getElementById("nvalores-1").innerHTML = "<img src='./img/quemsomos/FOTO1 valores.png '>";
+document.getElementById("nvalores-2").innerHTML = "<img src='./img/quemsomos/FOTO2 valores.png'>";
+document.getElementById("nvalores-3").innerHTML = "<img src='./img/quemsomos/FOTO3 valores.png'>";
+document.getElementById("nvalores-4").innerHTML = "<img src='./img/quemsomos/FOTO4 valores.png'>";
+document.getElementById("colab-1").innerHTML = "<img src='./img/quemsomos/FOTO1 parceria-colaboração.png'>";
+document.getElementById("colab-2").innerHTML = "<img src='./img/quemsomos/FOTO2 parceria-colaboração.png'>";
+
